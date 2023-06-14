295cb23885684c522afbf7376c16e7a12994a614
Albérico Junior <40373171+AlbericoJr@users.noreply.github.com>
2023-06-14 11:37:19 -0300

    MyLinks

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ function toggleMode(){
     const html = document.documentElement
     html.classList.toggle('light')
 
-    // Pegar a tag img
-
     const img = document.querySelector("#profile img")
 
-    // substituir a imagem
+    
     if(html.classList.contains('light')){
-        // se tiver light mode, adicionar a imagem light
+
         img.setAttribute('src', './assets/perfil-light.png')
     }else{
-        // se tiver sem light mode, manter a imagem normal
+        
         img.setAttribute('src','./assets/perfil-darck.png')
     }
 }
